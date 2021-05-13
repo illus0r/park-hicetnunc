c075a0920eb4e9a8ee1b29084387ca051fe51bfd
illus0r <zor667@gmail.com>
2021-05-14 02:55:51 +0300

    random 3d walker mesh!

--- index.js
+++ index.js
@@ -1,71 +1,83 @@
 'use strict';
-let twgl = require('twgl.js')
+let THREE = require('three')
 
-// setting up canvas
-let canvasP5, ctx
-let fShader
-let canvasGl
-let gl
-let program
-let positionObject
-let positionBuffer
+// console.log(THREE)
 
-let timeStart = +new Date();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-let sketch = function (p) {
-  p.setup = function () {
-    canvasP5 = p.createCanvas(512, 512).elt;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-    // shader stuff
-    setupShader()
-  }
 
-  p.draw = function () {
-    p.background(0);
-    p.fill(255);
-    p.circle(256 + p.sin(p.frameCount*.1) * 100, 256 + p.cos(p.frameCount*.1) * 100, 50);
 
-    // shader stuff
-    drawShader(p.frameCount)
-  };
+const geometry = new THREE.BufferGeometry();
+// create a simple square shape. We duplicate the top left and bottom right
+// vertices because each vertex needs to appear once per triangle.
+let vertices = []
+let indices = []
+let normals = []
+
+let floors = 30
+let segments = 8
+
+let offsetX = 0
+let offsetZ = 0
+for (let floor = 0; floor < floors; floor++) {
+    let r = .1 + .2 * Math.random()
+    offsetX += .2 - .4 * Math.random()
+    offsetZ += .2 - .4 * Math.random()
+    for (let i = 0; i < segments; i++) {
+        let angle = Math.PI / 4 * i
+        let x = r * Math.sin(angle) + offsetX
+        let y = floor * .2 - 4
+        let z = r * Math.cos(angle) + offsetZ
+        vertices.push(x, y, z)
+        normals.push(Math.sin(angle), 0, Math.cos(angle))
+    }
+}
+console.log(indices)
+
+for (let floor = 0; floor < floors - 1; floor++) {
+    let ring = []
+    for (let i = 0; i < segments; i++) {
+        ring.push(i + floor * segments)
+    }
+    for (let i = 0; i < segments; i++) {
+        indices.push(
+            ring[i] + segments,
+            ring[i],
+            ring[(i + 1) % segments],
+        )
+        indices.push(
+            ring[i] + segments,
+            ring[(i + 1) % segments],
+            ring[(i + 1) % segments] + segments,
+        )
+    }
+
+}
+
+geometry.setIndex(indices);
+geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+// geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+geometry.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
+
+const material = new THREE.MeshNormalMaterial({ wireframe: true });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    // mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
 };
-let myp5 = new p5(sketch);
 
-
-function setupShader() {
-  ctx = canvasP5.getContext("2d");
-  const vShader = `#version 300 es
-  precision mediump float;
-  in vec2 position;
-  void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-  }`
-  fShader = require('./shader.frag')
-  canvasGl = document.getElementById('gl')
-  gl = canvasGl.getContext('webgl2', { premultipliedAlpha: false })
-  twgl.addExtensionsToContext(gl)
-  program = twgl.createProgramInfo(gl, [vShader, fShader])
-  positionObject = { position: { data: [1, 1, 1, -1, -1, -1, -1, 1], numComponents: 2 } }
-  positionBuffer = twgl.createBufferInfoFromArrays(gl, positionObject)
-}
-
-function drawShader(tick) {
-  let texture = twgl.createTexture(gl, {
-    src: ctx.canvas,
-    format: gl.RGB,
-    min: gl.LINEAR,
-    wrap: gl.CLAMP_TO_EDGE,
-  })
-  twgl.resizeCanvasToDisplaySize(gl.canvas);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-  gl.useProgram(program.program)
-  twgl.setBuffersAndAttributes(gl, program, positionBuffer)
-  twgl.setUniforms(program, {
-    u_texture: texture,
-    u_tick: tick,
-    u_time: (new Date() - timeStart) / 1000,
-    u_resolution: [gl.canvas.clientWidth, gl.canvas.clientHeight],
-  })
-  twgl.bindFramebufferInfo(gl, null)
-  twgl.drawBufferInfo(gl, positionBuffer, gl.TRIANGLE_FAN)
-}
+animate();
